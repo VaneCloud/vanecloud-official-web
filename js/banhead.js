9e104369277b98c342d1a16d97251ecebeb0684b
Saw85 <2804265880@qq.com>
2016-07-06 09:37:55 +0800

    update project

--- js/banhead.js
+++ js/banhead.js
@@ -1,5 +1,33 @@
 $(function(){
-	var header='<div class="header"><div class="container"><div class="logo pull-left"></div><nav class="navHead pull-left"><li><a href="index.html">首页</a></li><li><a href="news.html">新闻动态</a><div class="navSection"><ul><li><a href="javascript:">国际动态</a></li><li><a href="javascript:">行业动态</a></li><li><a href="javascript:">氢氧动态</a></li><li><a href="javascript:">新品发布</a></li></ul></div></li><li><a href="javascript:">CSHO+</a></li><li><a href="javascript:">解决方案</a></li><li><a href="javascript:">体验中心</a></li></nav><hgroup class="logInOut pull-left"><li><a href="login.html">登录</a></li><li><a href="registry.html" class="active">注册</a></li></hgroup><div class="search pull-left"><input type="search" placeholder="搜索vaneWIKI" /><input type="button" /></div></div></div>';
+	var header='<div class="header">'+
+					'<div class="container">'+
+						'<div class="logo pull-left"></div>'+
+							'<nav class="navHead pull-left">'+
+								'<li><a href="index.html">首页</a></li>'+
+								'<li><a href="news.html">新闻动态</a>'+
+									'<div class="navSection">'+
+										'<ul>'+
+											'<li><a href="javascript:">国际动态</a></li>'+
+											'<li><a href="javascript:">行业动态</a></li>'+
+											'<li><a href="javascript:">氢氧动态</a></li>'+
+											'<li><a href="javascript:">新品发布</a></li>'+
+										'</ul>'+
+									'</div>'+
+								'</li>'+
+								'<li><a href="javascript:">CSHO+</a></li>'+
+								'<li><a href="javascript:">解决方案</a></li>'+
+								'<li><a href="javascript:">体验中心</a></li>'+
+							'</nav>'+
+							'<hgroup class="logInOut pull-left">'+
+								'<li><a href="login.html">登录</a></li>'+
+								'<li><a href="registry.html" class="active">注册</a></li>'+
+							'</hgroup>'+
+							'<div class="search pull-left">'+
+								'<input type="search" placeholder="搜索vaneWIKI" />'+
+								'<input type="button" />'+
+							'</div>'+
+						'</div>'+
+					'</div>';
 	$('.picture .pic').prepend(header);
 
 })
