8eaca28e332d9d6573786fe72220fca01580d588
Saw85 <2804265880@qq.com>
2016-07-15 15:27:26 +0800

    update project

--- js/treelog.js
+++ js/treelog.js
@@ -5,10 +5,16 @@ $(function(){
 			$('.logpicB').css('animation','1s logrotateb linear 0s forwards');
 			$('.logpic').css('animation','1s logpic linear 0s forwards');
 
+			$('.monitPic .pic > li:nth-child(1)').css('animation','1s monit1 linear 0s forwards');
+			$('.monitPic .pic > li:nth-child(2)').css('animation','1s monit2 linear 0s forwards');
+
 		}else{
 			$('.logpicT').css('animation','1s logrotateT linear 0s forwards');
 			$('.logpicB').css('animation','1s logrotateB linear 0s forwards');
 			$('.logpic').css('animation','1s logpicc linear 0s forwards');
+
+			$('.monitPic .pic > li:nth-child(1)').css('animation','1s monit11 linear 0s forwards');
+			$('.monitPic .pic > li:nth-child(2)').css('animation','1s monit22 linear 0s forwards');
 		}
 
 		
