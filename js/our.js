1fe36fe067da58e1badbb53551e84f864b9342da
Saw85 <2804265880@qq.com>
2016-07-22 16:34:02 +0800

    update project

---
+++ js/our.js
@@ -0,0 +1,67 @@
+$(function(){
+
+	var our='<div class="container">'+
+		'<ul class="row serviceTop">'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">首页</div>'+
+				'<ul class="serviceBody">'+
+					'<li><a href="javascript:">新闻动态</a></li>'+
+					'<li><a href="javascript:">关于我们</a></li>'+
+					'<li><a href="javascript:">行业解决方案</a></li>'+
+					'<li><a href="javascript:">产品体验中心</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">动态资讯</div>'+
+				'<ul class="serviceBody">'+
+					'<li><a href="javascript:">国际信息</a></li>'+
+					'<li><a href="javascript:">行业动态</a></li>'+
+					'<li><a href="javascript:">氢氧动态</a></li>'+
+					'<li><a href="javascript:">产品发布</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">CSHO+</div>'+
+				'<ul class="serviceBody">'+
+					'<li><a href="javascript:">中科氢氧</a></li>'+
+					'<li><a href="javascript:">云计算产品</a></li>'+
+					'<li><a href="javascript:">大数据产品</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">行业解决方案</div>'+
+				'<ul class="serviceBody">'+
+					'<li><a href="javascript:">金融</a></li>'+
+					'<li><a href="javascript:">政府</a></li>'+
+					'<li><a href="javascript:">教育</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">产品体验中心</div>'+
+				'<ul class="serviceBody">'+
+					'<li><a href="javascript:">氢氧云</a></li>'+
+					'<li><a href="javascript:">氢氧云盘</a></li>'+
+					'<li><a href="javascript:">氢氧云管平台</a></li>'+
+					'<li><a href="javascript:">氢氧智迅</a></li></a>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2">'+
+				'<div class="serviceTitle">关于我们</div>'+
+				'<div class="ma"><img src="images/ma.png" /></div>'+
+				'<ul class="serviceBody"><li><a href="javascript:">中科氢氧</a></li></ul>'+
+			'</li>'+
+		'</ul>'+
+		'<ul class="row serviceBot">'+
+			'<li class="col-lg-6 col-md-6">'+
+				'<ul class="xwq">'+
+					'<li><a href="javascript:"></a></li>'+
+					'<li><a href="javascript:"></a></li>'+
+					'<li><a href="javascript:"></a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li class="col-lg-2 col-md-2 col-lg-offset-4 col-md-offset-4 language">中文  En</li>'+
+		'</ul>'+
+	'</div>';
+
+	$('.service').append(our);
+})
---
+++ js/settleNav.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	var bgNav='<nav class="settleNav">'+
+		'<a href="finance.html" class="nav1">金融方案</a>'+
+		'<a href="education.html" class="nav2">教育方案</a>'+
+		'<a href="government.html" class="nav3">政府方案</a>'+
+		'<a href="retail.html" class="nav4">零售方案</a>'+
+		'<a href="bigData.html" class="nav5">大数据方案</a>'+
+	'</nav>';
+
+	$('#settleNav').append(bgNav);
+})
