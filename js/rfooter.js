4cb5b0e082a5085887954cf2cdb989791c07a0be
Saw85 <2804265880@qq.com>
2016-08-16 19:34:29 +0800

    update project

--- js/rfooter.js
+++ js/rfooter.js
@@ -5,12 +5,12 @@ var Foot= React.createClass({
     render: function() {
         return <div className="container">
             <ul className="version pull-left">
-            <li>contactus@vanecloud.com</li>
+            <li>www.vanecloud.com</li>
             <li>Tel:+86.21.6151 3468</li>
         </ul>
         <ul className="version pull-right">
             <li>中科氢氧云计算技术（上海）有限公司</li>
-            <li>www.vanecloud.com</li>
+            <li>Copyright © 2016 CSHO  Cloud (Shanghai) Co., Ltd. All rights reserved.</li>
         </ul>
         </div>;
     }
