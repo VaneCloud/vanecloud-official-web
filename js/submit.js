bf347ef861bac232109e46a64f791a4812e8fedd
Saw85 <2804265880@qq.com>
2016-08-19 17:01:16 +0800

    update project

--- js/submit.js
+++ js/submit.js
@@ -4,8 +4,11 @@
 
 $(function() {
     $('input.submit').on('click', function () {
+
         sendMail();
+
         return false;
+
     })
 
     function sendMail() {
@@ -28,7 +31,7 @@ $(function() {
 
         var demand=$('#demand').val();
 
-        var link='mailto:yuyan@vanecloud.com?subject='+encodeURIComponent("测试")+
+        var link='mailto:zhukai@vanecloud.com?subject='+encodeURIComponent("氢氧产品体验-个人信息填写")+
             '&body=姓名：'+username+'<br/>职位：'+job+'<br/>公司名称：'+company+'<br/>个人邮箱：'+
             email+'<br/>与氢氧的合作关系：'+relation+'<br/>所需产品：'+product+'<br/>所属行业：'+industry+
             '<br/>感兴趣产品：'+interest+'<br/>需求描述：'+demand;
